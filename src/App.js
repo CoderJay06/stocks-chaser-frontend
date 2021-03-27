f8e260bfa7f5ccf43151252e2867cf32e3b9d6e3
CoderJay06 <jaycru06@yahoo.com>
2021-03-27 04:38:33 -0400

    first commit frontend. Removed unused files/code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component() {
+   render() {
+      return (
+         <div className="App">
+            App
+         </div>
+       );
+   }
+
+}
+
+export default App;
