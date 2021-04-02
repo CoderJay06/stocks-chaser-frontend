723a4b4bc01acad9f408ad8831fb6b630c6b0c1f
CoderJay06 <jaycru06@yahoo.com>
2021-04-02 15:28:42 -0400

    Install react router, add links to app homepage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import StocksContainer from './containers/StocksContainer';
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   NavLink
+ } from "react-router-dom";
 
 class App extends Component {
 
-   // componentDidMount() {
-      // http://127.0.0.1:3000/api/v1/stocks
-   //    fetch('http://localhost:3000/api/v1/stocks', {
-   //       method: 'GET',
-   //       headers: {
-   //          'Content-Type': 'application/json',
-   //          'Accept': 'application/json'
-   //       }
-   //    })
-   //    .then(res => res.text())
-   //    .then(text => console.log(text))
-   // }
-
    render() {
       return (
-         <div className="App">
-            App
-            <StocksContainer />
-         </div>
+         <Router>
+            <div className="App">
+               <nav className="text-center bg-blue-900 text-white-100 p-4">
+                  <NavLink
+                     className="inline-block px-4 py-2"
+                     activeClassName="text-white-300"
+                     exact to="/stocks">
+                     Stocks
+                  </NavLink>
+                  <NavLink
+                     className="inline-block px-4 py-2"
+                     activeClassName="text-white-300"
+                     exact to="/signup">
+                     Signup
+                  </NavLink>
+                  <NavLink
+                     className="inline-block px-4 py-2"
+                     activeClassName="text-white-300"
+                     exact to="/login">
+                     Login
+                  </NavLink>
+               </nav>
+               <Switch>
+                  <Route exact path="/stocks">
+                     <StocksContainer />
+                  </Route>
+               </Switch>
+            </div>
+         </Router>
        );
    }
 
