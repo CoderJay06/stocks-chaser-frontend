d423802551ebcc585b649215d1da64a4308e25b7
CoderJay06 <jaycru06@yahoo.com>
2021-04-04 05:20:25 -0400

    Fix nested state update problem in signup, save users to db on submit

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -9,28 +9,68 @@ class SignupForm extends Component {
       }
    };
 
+   handleOnChange = event => {
+      // update state with user input
+      const { user } = {...this.state};
+      const currentUserState = user;
+      const newUserInput = event.target.value;
+      currentUserState[event.target.name] = newUserInput;
+
+      this.setState({ user: currentUserState });
+   }
+
+   handleOnSubmit = event => {
+      event.preventDefault();
+      const usersUrl = "http://localhost:3000/api/v1/users";
+      const configUsersObj = {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+         },
+         body: JSON.stringify(this.state)
+      };
+
+      // signup user, add them to users db
+      fetch(usersUrl, configUsersObj)
+         .then(response => response.json())
+      
+      // reset user state
+      this.setState({
+         user: {
+            email: '',
+            username: '',
+            password: ''
+         }
+      });
+   }
+
    render() {
       return (
          <div>
-            <form className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6">
-               <label htmlFor="email">Email</label>
+            <form className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6"
+                  onSubmit={this.handleOnSubmit}>
+               <label>Email</label>
                <input name="email" 
                       type="email" 
-                      value={this.state.user.email} 
+                      value={this.state.user.email}
+                      onChange={this.handleOnChange} 
                       placeholder="Enter your email"
                       className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200" />
                <br />
-               <label htmlFor="username">Username</label>
+               <label>Username</label>
                <input name="username" 
                       type="text" 
-                      value={this.state.user.username} 
+                      value={this.state.user.username}
+                      onChange={this.handleOnChange} 
                       placeholder="Enter a username"
                       className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200" />
                <br />
-               <label htmlFor="password">Password</label>
+               <label>Password</label>
                <input name="password" 
                       type="password" 
-                      value={this.state.user.password} 
+                      value={this.state.user.password}
+                      onChange={this.handleOnChange} 
                       placeholder="Enter a password"
                       className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200" />
                <br />
