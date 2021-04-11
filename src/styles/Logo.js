dced1cd4cda726bc8e9901cf27338b93e0c244fa
CoderJay06 <jaycru06@yahoo.com>
2021-04-11 04:46:58 -0400

    Add logo as background image

---
+++ src/styles/Logo.js
@@ -0,0 +1,9 @@
+
+export const logo = {
+      backgroundImage: "url(/images/logo.jpeg)", 
+      width: "1300px", 
+      height: "2500px", 
+      backgroundSize: "fit"
+};
+
+export default logo;
