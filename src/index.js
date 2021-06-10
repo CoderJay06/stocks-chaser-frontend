22bb34a725971d43495de9bf28aea0944cbf8528
CoderJay06 <jaycru06@yahoo.com>
2021-06-10 02:14:34 -0400

    Reinstall tailwind, update package json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux';
 import store from './store';
-
+import './styles/'
 import App from './App';
 
 
