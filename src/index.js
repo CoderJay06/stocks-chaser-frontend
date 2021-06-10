9e61c32e1a20cbd92aad52559fab745227d0a09c
CoderJay06 <jaycru06@yahoo.com>
2021-06-10 02:49:53 -0400

    Fix postcss8 plugin required error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import './styles/'
+import './styles/index.css'
 import App from './App';
 
 
