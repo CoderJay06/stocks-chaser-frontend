75bc755586c982ad3aaeb1048239059e90d7c0e3
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2022-01-22 05:12:27 -0500

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import IpoCalendar from './IPOCalendar'
+import React from "react";
+import IpoCalendar from "./IPOCalendar";
+import StocksContainer from "../containers/StocksContainer";
 
 const Home = () => {
-   return (
-      <div>
-            {/* <IpoCalendar /> */}
-      </div>
-   )
-}
+  return (
+    <div>
+      <IpoCalendar />
+      <StocksContainer />
+    </div>
+  );
+};
 
 export default Home;
